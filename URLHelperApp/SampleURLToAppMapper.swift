5596c521ecd0275debd38ff1622050e26643b750
Grigory Entin <grigory.entin@gmail.com>
2018-10-06 00:58:16 +0200

    Switched to asynchronous mapping; moved out SampleURLToAppMapper.

---
+++ URLHelperApp/SampleURLToAppMapper.swift
@@ -0,0 +1,31 @@
+//
+//  SampleURLToAppMapper.swift
+//  URLHelperApp
+//
+//  Created by Grigory Entin on 06/10/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    fileprivate func matchingAppBundleIdentifier() -> String {
+        switch self {
+        case _ where hasPrefix("https://stackoverflow.com/"):
+            return "org.epichrome.app.Coding"
+        default:
+            return "com.google.Chrome"
+        }
+    }
+}
+
+class SampleURLToAppMapper : URLToAppMapper {
+    
+    func appBundleIdentifierFor(_ url: URL, completionHandler: @escaping (Result<String>) -> Void) {
+        let urlString = url.absoluteString
+        let appBundleIdentifier = urlString.matchingAppBundleIdentifier()
+        
+        completionHandler(.success(appBundleIdentifier))
+    }
+}
