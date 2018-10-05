3c918e262436e1ba0b152d3174e02d18bc3afae3
Grigory Entin <grigory.entin@gmail.com>
2018-10-06 00:59:43 +0200

    Switched to asynchronous mapping; switched to ScriptBasedURLToAppMapper.

--- URLHelperApp/AppDelegate.swift
+++ URLHelperApp/AppDelegate.swift
@@ -8,30 +8,48 @@
 
 import Cocoa
 
-private let urlToAppMapper: URLToAppMapper = URLToAppMapperImp()
+private let urlToAppMapper: URLToAppMapper = ScriptBasedURLToAppMapper()
 
 @NSApplicationMain
 class AppDelegate : NSObject, NSApplicationDelegate {
     
     func application(_ application: NSApplication, open urls: [URL]) {
         dump(urls, name: "urls")
-        let urlsByAppBundleIdentifier: [String: [URL]] = urls.reduce(into: [:]) { urlsByAppBundleIdentifier, url in
-            let appBundleIdentifier = urlToAppMapper.appBundleIdentifierFor(url)
-            urlsByAppBundleIdentifier[appBundleIdentifier, default: []] += [url]
-        }
-        for (appBundleIdentifier, urls) in dump(urlsByAppBundleIdentifier, name: "urlsByAppBundleIdentifier") {
-            guard let appURL = workspace.urlForApplication(withBundleIdentifier: appBundleIdentifier) else {
-                dump(appBundleIdentifier, name: "appBundleIdentifierMissingApp")
-                continue
+        var urlsByAppBundleIdentifier: [String: [URL]] = [:]
+        let queryQueue = DispatchQueue.global()
+        let resultGroup = DispatchGroup()
+        let resultQueue = DispatchQueue(label: "")
+        urls.forEach { url in
+            resultGroup.enter()
+            queryQueue.async {
+                urlToAppMapper.appBundleIdentifierFor(url) { result in
+                    resultQueue.async {
+                        result.analysis(ifSuccess: { (appBundleIdentifier) in
+                            urlsByAppBundleIdentifier[appBundleIdentifier, default: []] += [url]
+                        }, ifFailure: { error in
+                            dump(error, name: "error")
+                            dump(url, name: "failingURL")
+                        })
+                        resultGroup.leave()
+                    }
+                }
             }
-            do {
-                let runningApp = try workspace.open(dump(urls, name: "urlsToOpenWithAppAtURL"), withApplicationAt: dump(appURL, name: "appURL"), options: .withErrorPresentation, configuration: [:])
-                dump(runningApp, name: "runningApp")
-            } catch {
-                dump(error, name: "openURLsError")
+        }
+        resultGroup.notify(queue: .main) {
+            for (appBundleIdentifier, urls) in dump(urlsByAppBundleIdentifier, name: "urlsByAppBundleIdentifier") {
+                guard let appURL = workspace.urlForApplication(withBundleIdentifier: appBundleIdentifier) else {
+                    dump(appBundleIdentifier, name: "appBundleIdentifierMissingApp")
+                    continue
+                }
+                do {
+                    let runningApp = try workspace.open(dump(urls, name: "urlsToOpenWithAppAtURL"), withApplicationAt: dump(appURL, name: "appURL"), options: .withErrorPresentation, configuration: [:])
+                    dump(runningApp, name: "runningApp")
+                } catch {
+                    dump(error, name: "openURLsError")
+                }
             }
         }
     }
-
-    private let workspace = NSWorkspace()
 }
+
+private let workspace = NSWorkspace()
