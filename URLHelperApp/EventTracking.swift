794d6faf6972f6d75ed76e8376b78f22469eb30d
Grigory Entin <grigory.entin@gmail.com>
2019-04-03 00:04:23 +0200

    Tweaked reporting to avoid () for otherwise dummy events.

--- URLHelperApp/EventTracking.swift
+++ URLHelperApp/EventTracking.swift
@@ -13,24 +13,24 @@ protocol Action {
 }
 
 enum ActionEvent<T: Action> {
-    case will(T, with: T.Input)
-    case succeeded(T, with: T.SuccessResult)
-    case failed(T, due: T.FailureResult)
+    case will(T.Type, with: T.Input)
+    case succeeded(T.Type, with: T.SuccessResult)
+    case failed(T.Type, due: T.FailureResult)
 }
 
 func track<T>(_ event: ActionEvent<T>) {
     print(event)
 }
 func track<T>(will action: T, with input: T.Input) where T: Action {
-    let event = ActionEvent<T>.will(action, with: input)
+    let event = ActionEvent<T>.will(type(of: action), with: input)
     print(event)
 }
 func track<T>(succeeded action: T, with result: T.SuccessResult) where T: Action {
-    let event = ActionEvent<T>.succeeded(action, with: result)
+    let event = ActionEvent<T>.succeeded(type(of: action), with: result)
     print(event)
 }
 func track<T>(failed action: T, due: T.FailureResult) where T: Action {
-    let event = ActionEvent<T>.failed(action, due: due)
+    let event = ActionEvent<T>.failed(type(of: action), due: due)
     print(event)
 }
 
