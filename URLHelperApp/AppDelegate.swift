2c5a5cfa785ff6aac7be31f3a73007f80dac036f
Grigory Entin <grigory.entin@gmail.com>
2019-04-02 08:30:23 +0200

    Segregated a few trackable actions.

--- URLHelperApp/AppDelegate.swift
+++ URLHelperApp/AppDelegate.swift
@@ -10,6 +10,7 @@ import GEAppConfig
 import GEFoundation
 import GETracing
 import Cocoa
+import Result
 
 extension TypedUserDefaults {
     
@@ -44,52 +45,101 @@ class AppDelegate : NSObject, NSApplicationDelegate {
 
     func application(_ application: NSApplication, open urls: [URL]) {
         x$(urls)
-        var urlsByAppBundleIdentifier: [String: [URL]] = [:]
-        let queryQueue = DispatchQueue.global()
-        let resultGroup = DispatchGroup()
-        let resultQueue = DispatchQueue(label: "")
-        urls.forEach { url in
-            resultGroup.enter()
-            queryQueue.async {
-                urlToAppMapper.appBundleIdentifierFor(url) { result in
-                    resultQueue.async {
-                        result.analysis(ifSuccess: { (appBundleIdentifier) in
-                            urlsByAppBundleIdentifier[appBundleIdentifier, default: []] += [url]
-                        }, ifFailure: { error in
-                            x$(error)
-                            x$(url)
-                        })
-                        resultGroup.leave()
-                    }
+        resolve(urls) { result in
+            result.analysis(ifSuccess: { (urlsByAppBundleIdentifier) in
+                for (appBundleIdentifier, urls) in urlsByAppBundleIdentifier {
+                    open(urls, withAppWithBundleIdentifier: appBundleIdentifier)
                 }
-            }
+            }, ifFailure: { error in
+                
+            })
         }
-        resultGroup.notify(queue: .main) {
-            for (appBundleIdentifier, urls) in x$(urlsByAppBundleIdentifier) {
-                switch defaults.openMethodValue ?? .default {
-                case .openURLsWithAppBundleIdentifier:
-                    workspace.open(urls, withAppBundleIdentifier: appBundleIdentifier, options: .withErrorPresentation, additionalEventParamDescriptor: nil, launchIdentifiers: nil)
-                case .openURLsWithApplicationAtURL:
-                    guard let appURL = workspace.urlForApplication(withBundleIdentifier: appBundleIdentifier) else {
-                        x$(appBundleIdentifier)
-                        continue
-                    }
-                    do {
-                        let runningApp = try workspace.open(x$(urls), withApplicationAt: x$(appURL), options: .withErrorPresentation, configuration: [:])
-                        x$(runningApp)
-                    } catch {
+    }
+}
+
+private func resolve(_ urls: [URL], completion: @escaping (Result<[String: [URL]], AnyError>) -> Void) {
+    
+    var urlsByAppBundleIdentifier: [String: [URL]] = [:]
+    let resultGroup = DispatchGroup()
+    let queryQueue = DispatchQueue.global()
+    let resultQueue = DispatchQueue(label: "")
+    urls.forEach { url in
+        resultGroup.enter()
+        queryQueue.async {
+            urlToAppMapper.appBundleIdentifierFor(url) { result in
+                resultQueue.async {
+                    result.analysis(ifSuccess: { (appBundleIdentifier) in
+                        urlsByAppBundleIdentifier[appBundleIdentifier, default: []] += [url]
+                    }, ifFailure: { error in
                         x$(error)
-                    }
+                        x$(url)
+                    })
+                    resultGroup.leave()
                 }
             }
         }
     }
+    resultGroup.notify(queue: .main) {
+        completion(.success(x$(urlsByAppBundleIdentifier)))
+    }
+}
+
+private func open(_ urls: [URL], withAppWithBundleIdentifier appBundleIdentifier: String) {
+    
+    switch defaults.openMethodValue ?? .default {
+    case .openURLsWithAppBundleIdentifier:
+        workspace.open(urls, withAppBundleIdentifier: appBundleIdentifier, options: .withErrorPresentation, additionalEventParamDescriptor: nil, launchIdentifiers: nil)
+    case .openURLsWithApplicationAtURL:
+        guard let appURL = resolveAppURL(forBundleIdentifier: appBundleIdentifier) else {
+            return
+        }
+        open(urls: urls, withAppAtURL: appURL)
+    }
+}
+
+private func open(urls: [URL], withAppAtURL appURL: URL) {
+    
+    struct OpenURLsWithAppAtURL : Action {
+        typealias Input = (urls: [URL], appURL: URL)
+        typealias SuccessResult = (NSRunningApplication)
+        typealias FailureResult = Error
+    }
+    let action = OpenURLsWithAppAtURL()
+    track(will: action, with: (urls: urls, appURL: appURL))
+    do {
+        let runningApp = try workspace.open(urls, withApplicationAt: appURL, options: .withErrorPresentation, configuration: [:])
+        track(succeeded: action, with: (runningApp))
+    } catch {
+        track(failed: action, due: error)
+    }
+}
+
+private func resolveAppURL(forBundleIdentifier bundleIdentifier: String) -> URL? {
+    
+    struct ResolveAppForBundleIdentifier : Action {
+        typealias Input = String
+        typealias SuccessResult = URL
+        typealias FailureResult = Error
+    }
+    enum Error: Swift.Error {
+        case couldNotLocateApplication(bundleIdentifier: String)
+    }
+    let action = ResolveAppForBundleIdentifier()
+    track(will: action, with: bundleIdentifier)
+    guard let appURL = workspace.urlForApplication(withBundleIdentifier: bundleIdentifier) else {
+        track(failed: action, due: Error.couldNotLocateApplication(bundleIdentifier: bundleIdentifier))
+        return nil
+    }
+    track(succeeded: action, with: appURL)
+    return appURL
 }
 
 private let workspace = NSWorkspace()
 
 let initializeDefaults: Void = {
+    #if false
 	traceEnabledEnforced = true
 	sourceLabelsEnabledEnforced = true
+    #endif
 	x$(())
 }()
